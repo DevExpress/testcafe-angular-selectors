2d5cae2b703b3289036d9308a761d4b6b44d375b
miherlosev@mail.ru <Ein7Onai>
2017-04-18 15:39:38 +0300

    Angular1Selector implemented

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
 # testcafe-angular-selectors
+
+This plugin provides selector extensions that make it easier to test Angular applications with [TestCafe](https://github.com/DevExpress/testcafe/).
+
+## Install
+
+```
+npm install testcafe-angular-selectors
+```
+
+## Angular1 Selector extensions
+
+### byBinding
+Find elements by text binding. Does a partial match, so any elements can be returned.
+```js
+Angular1Selector.byBinding(expression, parentSelector)
+```
+Parameter                   | Description
+--------------------------- | -----------
+expression                        |  The JavaScript expression to which the element's `textContent` is bound.
+parentSelector&#160;*(optional)*  | A TestCafe [selector](https://devexpress.github.io/testcafe/documentation/test-api/selecting-page-elements/selectors.html). If specified, TestCafe will search for the target element among the descendants of the element identified by this selector.
+
+NOTE: we don't support a deprecated syntax `Angular1Selector.byBinding('{{person.name}}')`
+
+### byExactBinding
+Find elements by exact binding.
+```js
+Angular1Selector.byExactBinding(expression, parentSelector)
+```
+Parameter                   | Description
+--------------------------- | -----------
+expression                        |  The JavaScript expression to which the element's `textContent` is bound.
+parentSelector&#160;*(optional)*  | A TestCafe [selector](https://devexpress.github.io/testcafe/documentation/test-api/selecting-page-elements/selectors.html). If specified, TestCafe will search for the target element among the descendants of the element identified by this selector.
+
+### byModel
+Find elements by 'ng-model' expression
+```js
+Angular1Selector.byModel(expression, parentSelector)
+```
+Parameter                   | Description
+--------------------------- | -----------
+expression                        |  The JavaScript expression to data-bind property of the scope to input,select, textarea (or custom form control).
+parentSelector&#160;*(optional)*  | A TestCafe [selector](https://devexpress.github.io/testcafe/documentation/test-api/selecting-page-elements/selectors.html). If specified, TestCafe will search for the target element among the descendants of the element identified by this selector.
+
+### byOptions
+                 
+Find elements by 'ng-options' expression.
+```js
+Angular1Selector.byOptions(expression, parentSelector)
+```
+Parameter                   | Description
+--------------------------- | -----------
+expression                        |  The JavaScript expression to dynamically generate a list of <option> elements for the <select> element
+parentSelector&#160;*(optional)*  | A TestCafe [selector](https://devexpress.github.io/testcafe/documentation/test-api/selecting-page-elements/selectors.html). If specified, TestCafe will search for the target element among the descendants of the element identified by this selector.
+
+### byRepeater
+Find elements by repeater. Does a partial match, so any elements can be returned.
+```js
+Angular1Selector.byRepeater(expression, parentSelector)
+```
+Parameter                   | Description
+--------------------------- | -----------
+expression                        |  The JavaScript expression to instantiate a template once per item from a collection.
+parentSelector&#160;*(optional)*  | A TestCafe [selector](https://devexpress.github.io/testcafe/documentation/test-api/selecting-page-elements/selectors.html). If specified, TestCafe will search for the target element among the descendants of the element identified by this selector.
+
+### byExactRepeat
+Find elements by exact repeater.
+```js
+Angular1Selector.byExactRepeat(expression, parentSelector)
+```
+Parameter                   | Description
+--------------------------- | -----------
+expression                        |  The JavaScript expression to instantiate a template once per item from a collection.
+parentSelector&#160;*(optional)*  | A TestCafe [selector](https://devexpress.github.io/testcafe/documentation/test-api/selecting-page-elements/selectors.html). If specified, TestCafe will search for the target element among the descendants of the element identified by this selector.
+             
+
+## Angular2 Selector extensions
+Angular2 component selectors is coming...
---
+++ src/angular1-selector.js
@@ -0,0 +1,119 @@
+/*global window*/
+
+import { Selector, ClientFunction } from 'testcafe';
+
+class Angular1Selector {
+    constructor () {
+        const getComparer            = ClientFunction(exactMatch => {
+            const strictComparer   = function (item1, item2) {
+                return item1 === item2;
+            };
+            const containsComparer = function (item1, item2) {
+                return item1.indexOf(item2) !== -1;
+            };
+
+            return exactMatch ? strictComparer : containsComparer;
+        });
+        const testNodeForAngularAttr = ClientFunction((node, attrSuffix) => {
+            const ANGULAR_ATTR_PREFIXES = ['ng-', 'ng_', 'data-ng-', 'x-ng-', 'ng\\:'];
+            const result                = {
+                hasAttr: false
+            };
+
+            for (let i = 0; i < ANGULAR_ATTR_PREFIXES.length; i++) {
+                const prefix   = ANGULAR_ATTR_PREFIXES[i];
+                const attrName = prefix + attrSuffix;
+
+                if (node.hasAttribute && node.hasAttribute(attrName)) {
+                    result.attrName = attrName;
+                    result.hasAttr  = true;
+                    break;
+                }
+            }
+
+            return result;
+        });
+        const ensureParent           = parentSelector => parentSelector || Selector(() => document.documentElement);
+
+        this._findByBinding     = (expression, parentSelector, exactMatch) => {
+            const parent       = ensureParent(parentSelector);
+            const dependencies = { getComparer, exactMatch, expression };
+            const filter       = ClientFunction(node => {
+                const dataBinding = window.angular.element(node).data('$binding');
+
+                if (!dataBinding)
+                    return false;
+
+                const bindingNameArr = dataBinding.exp || dataBinding[0].exp || dataBinding;
+                const bindingName    = Array.isArray(bindingNameArr) ? bindingNameArr[0] : bindingNameArr;
+                const comparer       = getComparer(exactMatch);
+
+                return comparer(bindingName, expression);
+            });
+
+            return parent
+                .find('.ng-binding')
+                .filter(filter, dependencies);
+        };
+        this._findByAngularAttr = (name, value, parentSelector, exactMatch) => {
+            const parent       = ensureParent(parentSelector);
+            const dependencies = { getComparer, testNodeForAngularAttr, exactMatch, name, value };
+            const filter       = ClientFunction(node => {
+                const result = testNodeForAngularAttr(node, name);
+
+                if (!result.hasAttr)
+                    return false;
+
+                const bindingAttrValue = node.getAttribute(result.attrName) || '';
+                const comparer         = getComparer(exactMatch);
+
+                return comparer(bindingAttrValue, value);
+            });
+
+            return parent.find(filter, dependencies);
+        };
+    }
+
+    _assertNonEmptyStringParameter (paramName, paramValue) {
+        if (typeof paramValue !== 'string' || !paramValue)
+            throw new Error(`"${paramName}" parameter should be a non-empty string`);
+    }
+
+    byBinding (expression, parentSelector) {
+        this._assertNonEmptyStringParameter('Binding expression', expression);
+
+        return this._findByBinding(expression, parentSelector);
+    }
+
+    byExactBinding (expression, parentSelector) {
+        this._assertNonEmptyStringParameter('Binding expression', expression);
+
+        return this._findByBinding(expression, parentSelector, true);
+    }
+
+    byModel (expression, parentSelector) {
+        this._assertNonEmptyStringParameter('Model expression', expression);
+
+        return this._findByAngularAttr('model', expression, parentSelector, true);
+    }
+
+    byOptions (expression, parentSelector) {
+        this._assertNonEmptyStringParameter('Options expression', expression);
+
+        return this._findByAngularAttr('options', expression, parentSelector, true);
+    }
+
+    byRepeater (expression, parentSelector) {
+        this._assertNonEmptyStringParameter('Repeat expression', expression);
+
+        return this._findByAngularAttr('repeat', expression, parentSelector);
+    }
+
+    byExactRepeater (expression, parentSelector) {
+        this._assertNonEmptyStringParameter('Repeat expression', expression);
+
+        return this._findByAngularAttr('repeat', expression, parentSelector, true);
+    }
+}
+
+export default new Angular1Selector();
