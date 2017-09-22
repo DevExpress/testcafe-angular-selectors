09f6fe876ea3be9d5125ff4ad051bb97f0436fda
miherlosev@mail.ru <Ein7Onai>
2017-09-22 14:08:25 +0300

    small fixes

--- test/data/angular/app/main.ts
+++ test/data/angular/app/main.ts
@@ -2,5 +2,4 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {enableProdMode} from '@angular/core';
 import {AppModule} from './app.module';
 
-//enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
