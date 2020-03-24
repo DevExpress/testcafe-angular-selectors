c187d266965d5468d03751c010768d60bdf53f30
Vladimir Airikh <vladimir.airich@gmail.com>
2020-03-24 16:57:47 +0300

    Apply @enten changes

---
+++ test/data/angular-9/app/app.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {ListComponent} from './list.component';
+import {ListItemComponent} from './list-item.component';
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent,
+        ListComponent,
+        ListItemComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
